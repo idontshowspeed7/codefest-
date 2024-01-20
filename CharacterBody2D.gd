9b40ba2766262181616103031cc007e42039ff4a
vishak.cretaceous@gmail.com <vishak.creatceous@gmail.com4>
2024-01-20 18:09:54 +0530

    added idle animation, jump punch kick

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -7,22 +7,26 @@ const JUMP_VELOCITY = -700.0
 # Gravity variable
 var gravity = 1900
 
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * _delta
 
 	# Handle jump and attacks.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		sprite_2d.play("jump")
 		velocity.y = JUMP_VELOCITY
+	
 
-	# Check if the "punch" animation exists in the AnimationPlayer.
-		if Input.is_action_just_pressed("ui_accept"):
-			# Play the "punch" animation
+	if Input.is_action_just_pressed("ui_accept"):
 			sprite_2d.play("punch")
-
+	if Input.is_action_just_pressed("ui_down"):
+			sprite_2d.play("kick")
+			
+	else: 
+		sprite_2d.play("default")
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
