829bce4c9dc2aea232c66d823248e40b5698ae15
vishak.cretaceous@gmail.com <vishak.creatceous@gmail.com4>
2024-01-20 17:28:56 +0530

    added kick sprite

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-
 const SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 @onready var sprite_2d = $Sprite2D
 
 # Gravity variable
 var gravity = 1900
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,8 +15,11 @@ func _physics_process(delta):
 	# Handle jump and attacks.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("ui_accept"):
-		sprite_2d.animation = "punch"
+
+	# Check if the "punch" animation exists in the AnimationPlayer.
+		if Input.is_action_just_pressed("ui_accept"):
+			# Play the "punch" animation
+			sprite_2d.play("punch")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,8 +30,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, 14)
 
 	move_and_slide()
-	
-	var isleft = velocity.x < 0
-	sprite_2d.flip_h = isleft
-	
-	
+
+	# Flip the sprite based on the movement direction.
+	var is_left = velocity.x < 0
+	sprite_2d.flip_h = is_left
