0a1bce4ea72128cce8849d8016569f377cd1ae1a
vishak.cretaceous@gmail.com <vishak.creatceous@gmail.com4>
2024-01-20 20:04:39 +0530

    added run animation and debugged

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,50 +3,44 @@ extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 @onready var sprite_2d = $Sprite2D
+@onready var animation_name = $CharacterBody2D
 
 # Gravity variable
 var gravity = 1900
 
 # Keep track of the current animation state
-var current_animation = "idle"
+var current_animation = "default"
 
 func _physics_process(_delta):
-	#default and run
-	if (velocity.x >1 || velocity.x <-1):
-		play_animation ("run")
-	else:
-		play_animation ("default")
 	
-	# Check for jump and attacks.
-		if Input.is_action_just_pressed("ui_up") and is_on_floor():
-			play_animation("jump")
-		velocity.y = JUMP_VELOCITY
 		if Input.is_action_just_pressed("ui_accept"):
-			play_animation("punch")
+			sprite_2d.play("punch")
 		if Input.is_action_just_pressed("ui_down"):
-			play_animation("kick")
+			sprite_2d.play("kick")
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+				sprite_2d.play("jump")
+		if (velocity.x > 1 || velocity.x < -1):
+				sprite_2d.play("run")
+
 	
 
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * _delta
-
+		if not is_on_floor():
+			velocity.y += gravity * _delta
+		
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 14)
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 14)
 
-	move_and_slide()
+		move_and_slide()
 
 	# Flip the sprite based on the movement direction.
-	var is_left = velocity.x < 0
-	sprite_2d.flip_h = is_left
+		var is_left = velocity.x < 0
+		sprite_2d.flip_h = is_left
 
-# Function to play animations
-func play_animation(animation_name: String) -> void:
-	# Check if the requested animation is different from the current one
-	if current_animation != animation_name:
-		sprite_2d.play(animation_name)
-		current_animation = animation_name
+
+			
