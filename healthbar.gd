5595ae68351f43056104c3ee63d071c059111aed
vishak.cretaceous@gmail.com <vishak.creatceous@gmail.com4>
2024-01-21 02:50:16 +0530

    added menu with non functional buttons

--- healthbar.gd
+++ healthbar.gd
@@ -1,1 +1,16 @@
-extends TextureProgressBar
+extends CharacterBody2D
+
+@onready var health_bar = $HealthBar  # Reference to the HealthBar node
+
+func _ready():
+	pass
+
+# Call this function when the player takes damage
+func take_damage(damage : int):
+	health_bar.take_damage(damage)
+
+# Call this function when the player heals
+func heal(healing : int):
+	health_bar.heal(healing)
+
+
