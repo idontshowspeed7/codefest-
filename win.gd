b7ac8c82818e1102fb9a54bda1e618e41a6f48af
vishak.cretaceous@gmail.com <vishak.creatceous@gmail.com4>
2024-01-21 10:13:18 +0530

    final minor adjustments

--- win.gd
+++ win.gd
@@ -4,3 +4,7 @@ extends TextureRect
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
 	
+
+
+func _on_rematch_pressed():
+	get_tree().change_scene_to_file("res://node.tscn")
